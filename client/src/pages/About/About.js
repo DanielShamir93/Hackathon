753f1d8c8ff6ba063440a980907c9b52246222e5
revital-m <revital13m@gmail.com>
2022-01-30 21:19:22 +0200

    added design for the text

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -16,12 +16,22 @@ function About() {
       <div class="box box-5"></div>
       <div class="box box-6"></div>
       <div class="box box-7"></div>
-      <div className="about--text box-8">{"an app that enables you to check on spot (according to the day’s date) what special event occurring in other people’s lives in your work space, or study program or even simply in any social event that includes diversity of cultures & religions so people can get closer together & respect each other & to bridge the social gap in the Israeli community. "}</div>
+      <ul className="about--text box-8">
+        <li>{"An app that enables you to check on spot (according to the day’s date) what special event occurring in other people’s lives in your work space, study program or even simply in any social event that includes diversity of cultures & religions."}</li>
+        <li>{"So people can get closer together & respect each other & bridge the social gap in the Israeli community."}</li>
+      </ul>
+      {/* <div className="about--text box-8">{"An app that enables you to check on spot (according to the day’s date) what special event occurring in other people’s lives in your work space, study program or even simply in any social event that includes diversity of cultures & religions. So people can get closer together & respect each other & bridge the social gap in the Israeli community. "}</div> */}
       <div class="box box-9"></div>
       <div class="box box-10"></div>
       <div class="box box-11"></div>
       <div class="box box-12"></div>
-      <div className="about--text box-13">{"We are 6 group members who met through a programming bootcamp, our values & identities stem from different roots of religions & cultures together with our other class mates.\nSo, this week’s mission was to find an idea that represents & connects us all on a deeper level.\nOur team:\nDaniel Shamir,\tFida Shnakher,\tOri Altarace,\tRevital maor,\tShaked Ben-Ratzon,\tTahrer Abu-Diab."}</div>
+      <ul className="about--text box-13">
+        <li>{"We are 6 group members who met through a programming bootcamp."}</li>
+        <li>{"Our values & identities stem from different roots of religions & cultures together with our other class mates."}</li>
+        <li>{"So, this week’s mission was to find an idea that represents & connects us all on a deeper level."}</li>
+        <li>{"Our team: Daniel Shamir, Fida Shnakher, Ori Altarace, Revital maor, Shaked Ben-Ratzon, Tahrer Abu-Diab."}</li>
+      </ul>
+      {/* <div className="about--text box-13">{"We are 6 group members who met through a programming bootcamp. Our values & identities stem from different roots of religions & cultures together with our other class mates.So, this week’s mission was to find an idea that represents & connects us all on a deeper level.Our team: Daniel Shamir, Fida Shnakher, Ori Altarace, Revital maor, Shaked Ben-Ratzon, Tahrer Abu-Diab."}</div> */}
       <div class="box box-14"></div>
     </div>
   </div>);
