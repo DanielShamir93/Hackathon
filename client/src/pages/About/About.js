ed75ab3ae855e5f9f2dce9a7d27f444d4c7d77c1
revital-m <revital13m@gmail.com>
2022-01-30 19:32:05 +0200

    added grid and imgs

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -2,7 +2,28 @@ import React from 'react';
 import './About.css';
 
 function About() {
-  return <div>About</div>;
+  return (<div className="about">
+    <div className="grid-container">
+      {/* <ul className="about--text box-1">
+        <li>{"Every week comes with multiple events from mixed cultures & religions."}</li>
+        <li>{"Our website explains you every single event in details & with relevant information including a video-describing the traditional way of celebrating the event."}</li>
+      </ul> */}
+      <div className="about--text box-1">{"Every week comes with multiple events from mixed cultures & religions.\nOur website explains you every single event in details & with relevant information including a video-describing the traditional way of celebrating the event."}</div>
+      <div class="box box-2"></div>
+      <div class="box box-3"></div>
+      <div class="box box-4"></div>
+      <div class="box box-5"></div>
+      <div class="box box-6"></div>
+      <div class="box box-7"></div>
+      <div className="about--text box-8">{"an app that enables you to check on spot (according to the day’s date) what special event occurring in other people’s lives in your work space, or study program or even simply in any social event that includes diversity of cultures & religions so people can get closer together & respect each other & to bridge the social gap in the Israeli community. "}</div>
+      <div class="box box-9"></div>
+      <div class="box box-10"></div>
+      <div class="box box-11"></div>
+      <div class="box box-12"></div>
+      <div className="about--text box-13">{"We are 6 group members who met through a programming bootcamp, our values & identities stem from different roots of religions & cultures together with our other class mates.\nSo, this week’s mission was to find an idea that represents & connects us all on a deeper level.\nOur team:\nDaniel Shamir,\tFida Shnakher,\tOri Altarace,\tRevital maor,\tShaked Ben-Ratzon,\tTahrer Abu-Diab."}</div>
+      <div class="box box-14"></div>
+    </div>
+  </div>);
 }
 
 export default About;
